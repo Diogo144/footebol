05f5287c90d5e0a542f91f5f39bbe3798bf84d8a
valbert diogo <valbertdiogo9@gmail.com>
2026-05-06 17:53:50 -0300

    Add files via upload

---
+++ footebol/public/public/public/firebase.js
@@ -0,0 +1,13 @@
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-app.js";
+
+export const firebaseConfig = {
+  apiKey: "AIzaSyBcnasVOKDdLbxe80A_n044SEEVwtSPI0k",
+  authDomain: "footebol-708ca.firebaseapp.com",
+  projectId: "footebol-708ca",
+  storageBucket: "footebol-708ca.firebasestorage.app",
+  messagingSenderId: "476560251323",
+  appId: "1:476560251323:web:07a98d069e37ca4ee5cc9e",
+  measurementId: "G-7XPFSTJ10N"
+};
+
+export const app = initializeApp(firebaseConfig);
